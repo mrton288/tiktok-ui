81d77e7654da4b86de5742bb868543351769f32a
nduc2808 <nd280801@gmail.com>
2022-09-06 08:39:17 +0700

    Xây dựng UI phần sidebar #3 -> add

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,7 +1,14 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu';
-import { HomeIcon, UserGroupIcon, LiveIcon, HomeActiveIcon, UserGroupActiveIcon, LiveActiveIcon,  } from '~/components/Icons';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    HomeActiveIcon,
+    UserGroupActiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
 
 import config from '~/config';
 import SuggestedAccounts from '~/components/SuggestedAccounts';
@@ -12,16 +19,17 @@ function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <Menu>
-                <MenuItem title="For you" to={config.routes.home} icon={<HomeIcon/>} activeIcon={<HomeActiveIcon/>}/>
-                <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon/>} activeIcon={<UserGroupActiveIcon/>}/>
-                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon/>} activeIcon={<LiveActiveIcon/>}/>
+                <MenuItem title="For you" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
             </Menu>
 
-            <SuggestedAccounts label='Suggested accounts'/>
-            <SuggestedAccounts label='Suggested accounts'/>
-
-            <SuggestedAccounts label='Suggested accounts'/>
-
+            <SuggestedAccounts label="Suggested accounts" />
         </aside>
     );
 }
